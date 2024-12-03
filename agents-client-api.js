5fa7ad9c66d7dc634aa34c396d6b15daa2cf63a0
Jeff-Li123 <ljf632098407@outlook.com>
2024-12-04 00:30:26 +0900

    更改原有30s视频为7s 过程待机动画

--- agents-client-api.js
+++ agents-client-api.js
@@ -210,7 +210,7 @@ function playIdleVideo() {
   videoElement.classList.toggle("animated")
 
   videoElement.srcObject = undefined;
-  videoElement.src = 'resource/WeChat_241019.mp4';
+  videoElement.src = 'resource/did_cutscene_7sec.mp4';
   videoElement.loop = true;
 
   // Remove Animation Class after it's completed
